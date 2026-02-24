4ee6536221d17dff7c6f37709a8aea8b5ba2b4d1
Sneha <sneharaghu803@gmail.com>
2026-02-24 16:41:12 +0530

    Add files via upload

---
+++ SpringElExp1/src/main/java/com/test/TestDemo.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
+		Student std=ctx.getBean("std", Student.class);
+		System.out.println(std);
+		
+		ExpressionParser exp=new SpelExpressionParser();
+		
+		Expression ex=exp.parseExpression("'Simple MLA coders Spring EL examples'");
+		String data=ex.getValue(String.class);
+		System.out.println(data+ " literals");
+		
+		Expression ex1=exp.parseExpression("'Simple MLA coders Spring EL examples'".toUpperCase());
+		String su=ex1.getValue(String.class);
+		System.out.println(su+ " uppercase method");
+		
+		Expression ex3=exp.parseExpression("100*5");
+		Integer v1=ex3.getValue(Integer.class);
+		System.out.println(v1);
+	}
+
+}
